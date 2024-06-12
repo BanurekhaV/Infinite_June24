b95efa265a62eb161c4ad1330f27871f1389c4b9
BanurekhaV <rekhasairam@hotmail.com>
2024-06-12 18:27:59 +0530

    First program

---
+++ CSharp/ConsoleApp_basic/ConsoleApp_basic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello all, welcome..");
+            Console.Read();
+        }
+    }
+}
