1fd3b2e0804b7d62d35d9c582326601ea479b016
BanurekhaV <rekhasairam@hotmail.com>
2024-06-13 17:01:37 +0530

    day1 csharp

---
+++ CSharp/Day1_Dotnet/Day1_Dotnet/NullableEg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_Dotnet
+{
+    class NullableEg
+    {
+       public static void nullexample()
+        {
+            //int ? data1 = null;
+
+            //int data2;
+            //if (data1 == null)
+            //{
+            //    data2 = 0;
+            //}
+            //else
+            //    data2 = (int)data1;  //or
+
+            ////  data2 = data1.Value;
+            //Console.WriteLine(data2);
+
+            //null coalescing operator to replace the above code
+            int ? data1 = 100;
+            int data2 = data1 ?? 0;
+            Console.WriteLine(data2);
+        }
+    }
+}
--- CSharp/Day1_Dotnet/Day1_Dotnet/TypeConversion.cs
+++ CSharp/Day1_Dotnet/Day1_Dotnet/TypeConversion.cs
@@ -10,18 +10,81 @@ namespace Day1_Dotnet
     {
         static void Main()
         {
+            // simpleConversion();
+            //  boxing_unboxing();
+            //TypeConversion tc = null;
+            //tc = new TypeConversion();
+            //bool res = tc.ternaryfunction();
+            //Console.WriteLine(tc.ternaryfunction());
+            NullableEg.nullexample();
+            Console.Read();
+        }
+             
+       bool ternaryfunction()
+        {
+            int num = 10;  //assigning
+            bool b;
+            /* if (num == 10) //checking for a specific value
+             {
+                 b = true;
+             }
+             else
+                 b = false;
+             //using ternary function
+
+             */
+            b = num == 10 ? true : false;
+            return b;
+        }
+        static void boxing_unboxing()
+        {
+            int x = 10;  //Value type
+            object obj;  // reference type
+            obj = x;   //value type to reference type - boxing
+            Console.WriteLine($"Value of x {x} ");
+            Console.WriteLine($" Value of obj {obj}");
+            string s = "AAA";  //reference type
+            obj = s; //same type
+            Console.WriteLine(obj);
+            float salary;  // value type
+            Console.WriteLine("Please enter your salary");
+           // salary = float.Parse(Console.ReadLine());  // string to float conversion - unboxing
+            salary = Convert.ToSingle(Console.ReadLine());
+            
+            Console.WriteLine(salary);
+
+        }
+        static void simpleConversion()
+        {
             int i = 100;
             Console.WriteLine("The value of int :" + i);
             float f = i;  //implicit conversion
             Console.WriteLine("The value of float :" + f);
 
-            f = 123.45f;
+            f = 12323456.45f;
             i = (int)f;  //explicit conversion using typecast
             Console.WriteLine("----------------");
             Console.WriteLine(i);
             i = Convert.ToInt32(f); //explicit conversion using conversion function
             Console.WriteLine(i);
-            Console.Read();
+
+            //using parse and tryparse()
+
+            string str = "100";
+            //  i = int.Parse(str);  // will convert the string into int
+            //  Console.WriteLine("**********");
+            Console.WriteLine(i);
+
+            // i = int.Parse(str); // throws an exception as the string has non number values also
+            // we can use tryparse() instead of parse to avoid exception
+            int result = 0;
+            bool success = int.TryParse(str, out result);
+            if (success)
+            {
+                Console.WriteLine(result);
+            }
+            else
+                Console.WriteLine("Invalid Data, could not parse..");
         }
     }
 }
