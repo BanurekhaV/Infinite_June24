d21734fb8e2441ac919a326ecabb70f6fd20a64b
BanurekhaV <rekhasairam@hotmail.com>
2024-06-17 18:27:57 +0530

    day3 dot net

---
+++ CSharp/Day3_Dotnet/Day3_Dotnet/Employee.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Dotnet
+{
+    class Employee
+    {
+        //fields
+        int Empid;
+        string EmpName;
+        DateTime DOJ;
+        float Salary;
+        
+        //1. constructor
+        public Employee()
+        {
+            Empid = 10;
+            EmpName = "Infinite Ltd.";
+            DOJ = Convert.ToDateTime("20/2/2024");  //dd/mm/yyyy
+            Salary = 50000;
+        }
+
+        //2. constructor with arguments/parameters (Parameterized constructor)
+        public Employee(int eid, string name, float sal)
+        {
+            Empid = eid;
+            EmpName = name;
+            Salary = sal;
+        }
+
+        //3. constructor with the usage of  'this' keyword
+      internal Employee(int Empid, string EmpName)
+        {
+           this.Empid = Empid;
+           this.EmpName = EmpName;
+        }
+        //methods/functions
+
+        public void GetEmployeeDetails()
+        {
+            Console.WriteLine("Enter id, name, doj and sal :");
+            Empid = Convert.ToInt32(Console.ReadLine());
+            EmpName = Console.ReadLine();
+            DOJ = Convert.ToDateTime(Console.ReadLine());
+            Salary = Convert.ToSingle(Console.ReadLine());
+        }
+
+       public void ShowEmpDetails()
+        {
+            Console.WriteLine($"Employee id{Empid}, Employee Name {EmpName}, DOJ {DOJ} and Salary is {Salary}");
+        }
+
+        //destructor
+        ~Employee()
+        {
+            Console.WriteLine("Bye from Employee..");
+            Console.Read();
+        }
+    }
+
+    class EmployeeTest
+    {
+        public static void Main()
+        {
+            Employee employee = new Employee(); //invoking empty constructor
+          //  employee.GetEmployeeDetails();
+            employee.ShowEmpDetails();
+            employee = null;
+            GC.Collect(); //forces to clear the reference memory immediately
+            Console.WriteLine("------------");
+            Employee employee2 = new Employee(20,"Pratheeba",25000); // invoking a construtor with 3 parameters
+          //  employee2.GetEmployeeDetails();
+            employee2.ShowEmpDetails();
+            Employee employee3 = new Employee(30, "Nithin");
+            Console.Read();
+
+        }
+    }
+}
---
+++ CSharp/Day3_Dotnet/Day3_Dotnet/Readonly_Constant_StaticEg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3_Dotnet
+{
+    class Readonly_Constant_StaticEg
+    {
+        //readonly variables declaration
+        public readonly int myvar1;  //assigning value to a readonly at the time of decl. for each object but cannot modified
+        public readonly int mydata;
+        int x; //non static member, available for each object and can be modified
+        const float PI = 3.14f; //constant member, 1 per class not modifiable
+        static int y = 200; //static member, 1 per class and modifiable
+
+
+
+        //constructor
+        public Readonly_Constant_StaticEg()
+        {
+            Console.WriteLine(myvar1);
+            myvar1 = 20;
+           // mydata = "Readonly Variable";
+            Console.WriteLine("Value of Myvar is {0}, and mydata is :{1}", myvar1, mydata);
+        }
+
+        public static void Main()
+        {
+            Readonly_Constant_StaticEg rcs = new Readonly_Constant_StaticEg();
+            Console.WriteLine(rcs.myvar1 + " " + rcs.mydata);
+            Readonly_Constant_StaticEg rcs2 = new Readonly_Constant_StaticEg();
+            Console.WriteLine("Rcs2 data " + rcs2.mydata + " " + rcs2.myvar1);
+            Console.WriteLine(PI);
+           // rcs.myvar1 = 1;  //not possible
+            rcs.x = 10;
+            rcs2.x = 100;
+            Console.WriteLine($"X is {rcs.x}, Y is {y}, Myvar1 is {rcs.myvar1}, and Constant is {PI} ");
+            // PI = 6.6f;  //not possible
+            Readonly_Constant_StaticEg.y = 300;
+            y = 500;
+            Console.WriteLine($"X is {rcs.x}, Y is {y}, Myvar1 is {rcs.myvar1}, and Constant is {PI} ");
+            Console.Read();
+        }
+    }
+}
