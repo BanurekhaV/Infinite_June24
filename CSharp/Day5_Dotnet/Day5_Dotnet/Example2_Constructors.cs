6c8e6ace5c19b761dce8b60333c0d463b2414d3c
BanurekhaV <rekhasairam@hotmail.com>
2024-06-20 11:36:59 +0530

    constructor chain

---
+++ CSharp/Day5_Dotnet/Day5_Dotnet/Example2_Constructors.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Dotnet
+{
+    class Parent
+    {
+        int ID;
+        public Parent(int x):this()
+        {
+            ID = x;
+            Console.WriteLine(ID);
+        }
+
+        public Parent()
+        {
+            Console.WriteLine("This is another constructor of the Parent");
+        }
+    }
+
+    class Child : Parent
+    {
+        string Name;
+        public Child(string s, int a):base(a)
+        {
+            Name = s;
+            Console.WriteLine(Name);
+        }
+    }
+    class Example2_Constructors
+    {
+        public static void Main()
+        {
+
+            //  Parent p = new Parent(6);
+            //  Child c = new Child("Hai",8);
+            DerivedClass dc = new DerivedClass("Message"); // chaining of constructores in the baseclass
+            Console.WriteLine("-------------------"); //
+            DerivedClass dc1 = new DerivedClass(55);
+            Console.Read();
+        }
+    }
+}
---
+++ CSharp/Day5_Dotnet/Day5_Dotnet/TestClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_Dotnet
+{
+    class BaseClass
+    {
+        //parametrized constructor
+        public BaseClass(string s):this(2,2.5f)
+        {
+            Console.WriteLine("Base " + s);
+        }
+
+        public BaseClass(int x, float f):this()
+        {
+            Console.WriteLine(x +  " " + f);
+        }
+        public BaseClass()
+        {
+            Console.WriteLine("Instantiated thru Empty constructor ");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        public DerivedClass(string str):base(str)
+        {
+            Console.WriteLine("Derived");
+        }
+
+        public DerivedClass()
+        {
+        Console.WriteLine("Empty Derived");
+        }
+
+        public DerivedClass(int x):this()
+        {
+            Console.WriteLine(x);
+        }
+    }
+}
