e97f7eda2c9c00ed8580b41709886ceaede79111
BanurekhaV <rekhasairam@hotmail.com>
2024-06-21 17:33:27 +0530

    day6 dotnet

---
+++ CSharp/Day7_Dotnet/Day7_Dotnet/EqualsMethodEg.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using static System.Console;
+
+namespace Day7_Dotnet
+{
+    class EqualsMethodEg
+    {
+        public enum Direction { East, West, North, South};
+        public static void Main()
+        {
+            //equality operator with value types
+            //int number1 = 10;
+            //int number2 = 10;
+            //WriteLine($"Number 1 == Number2 : {number1 == number2}");
+            //WriteLine($"Number1.Equals(Number2) ?: {number1.Equals(number2)}");
+            //Console.WriteLine("--------------------");
+
+            //Direction d1 = Direction.East;
+            //Direction d2 = Direction.East;
+
+            //WriteLine(d1 == d2);
+            //WriteLine(d1.Equals(d2));
+            WriteLine("*************************");
+            Equality_With_ReferenceTypes();
+            Console.Read();
+        }
+
+        public static void Equality_With_ReferenceTypes()
+        {
+            Customers c1 = new Customers();
+            c1.FirstName = "Raviteja";
+            c1.LastName = "Booraga";
+
+            Customers c2 = c1;
+            WriteLine($"C1 == C2 : {c1 == c2} {c1.GetHashCode()} and {c2.GetHashCode()}");
+            WriteLine($"C1.Equals(C2) : {c1.Equals(c2)}");
+
+            Console.WriteLine("--------------------");
+            c2 = new Customers();
+            c2.FirstName = "Raviteja";
+            c2.LastName = "B";
+
+            WriteLine($"C1 == C2 : {c1 == c2} {c1.GetHashCode()} and {c2.GetHashCode()}");
+            WriteLine($"C1.Equals(C2) : {c1.Equals(c2)}");
+
+        }
+    }
+
+    public class Customers
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            //ensure that the passed object is not null
+            if (obj == null)
+            {
+                return false;
+            }
+
+            //ensure that the passed object is of type Customers
+            if (!(obj is Customers))
+            {
+                return false;
+            }
+
+            //we are changing the implementation code of Equals() method
+            //of the 'Object' Class to compare the values of 2 objects and not the references
+            return (this.FirstName == ((Customers)obj).FirstName) &&
+                (this.LastName == ((Customers)obj).LastName);
+        }
+
+        public override int GetHashCode()
+        {
+            return FirstName.GetHashCode() ^ LastName.GetHashCode();
+        }
+    }
+}
---
+++ CSharp/Day7_Dotnet/Day7_Dotnet/Interface_WithProperties.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_Dotnet
+{
+    interface IName
+    {
+        string Name { get; set; }
+    }
+
+    class Employee : IName
+    {
+        public string Name { get; set; }  //class employee has implemented the property of IName
+    }
+
+    class Company : IName
+    {
+        private string _company; // declaring a private field member
+        public int _age { get; set; }
+
+        public string Name
+        {
+            get { return _company; }
+            set { _company = value; }
+        }
+
+    }
+    class Interface_WithProperties
+    {
+        static void Main()
+        {
+            IName Emp = new Employee();
+            Emp.Name = "Vijendra";
+
+            IName Comp = new Company();
+            Comp.Name = "Infinite";  // inherited property of the IName interface
+
+            Company c = new Company();
+            c.Name = "Infinite Ltd.,";
+            c._age = 20;  //local property of the company class
+            Console.WriteLine($"Employee Named {Emp.Name} works for {Comp.Name} which is {c._age} years old and the new Name is {c.Name}");
+
+            Console.Read();
+
+        }
+    }
+    
+}
