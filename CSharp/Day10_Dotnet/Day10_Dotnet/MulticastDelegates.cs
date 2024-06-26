a20168285761762c87dc751ddcb18b310ae97b24
BanurekhaV <rekhasairam@hotmail.com>
2024-06-26 11:28:36 +0530

    day10 Dotnet

---
+++ CSharp/Day10_Dotnet/Day10_Dotnet/MulticastDelegates.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_Dotnet
+{
+    class MulticastDelegates
+    {
+        public delegate void Multicastdel();
+        public static void Main()
+        {
+            //one way of multicasting
+            //Multicastdel m1, m2, m3, m4;
+            //m1 = new Multicastdel(Method1);
+            //m2= new Multicastdel(Method2);
+            //m3= new Multicastdel(Method3);
+            //m4 = m1 + m2 + m3;  // adding multiple delegate objects
+            //m4();
+
+            //m4 -= m2;   //removing m2 delegate from the multicast 
+            ////or m4 = m1+m3-m2
+            //m4();
+
+            //2nd way to multicast
+            Multicastdel md1 = new Multicastdel(Method1);
+            md1 += Method2;
+            md1 += Method3;
+            md1();
+            Console.WriteLine("-----------");
+            md1 -= Method1;
+            md1();
+
+            Console.WriteLine("Multicast with return values .......");
+            //creating the object of the class which implements functions with int return type
+            MulticastDelegatesWithReturn mdr = new MulticastDelegatesWithReturn();
+            NumberChanger numc = new NumberChanger(mdr.Square);
+            numc += mdr.Triple;
+            int result = numc(5);
+            Console.WriteLine("The return value is {0}", result);
+            Console.Read();
+        }
+
+        public static void Method1()
+        {
+            Console.WriteLine("Method 1..");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Method 2..");
+        }
+        public static void Method3()
+        {
+            Console.WriteLine("Method 3..");
+        }
+    }
+
+    class MulticastDelegatesWithReturn
+    {
+        static int p;
+
+        public int Square(int x)
+        {
+            p = x * x;
+            Console.WriteLine(p + " is the current value of p");
+            return p;
+        }
+
+        public int Triple(int q)
+        {
+            p = q * q * q;
+            Console.WriteLine(p + " is the current value of p");
+            return p;
+        }
+    }
+}
---
+++ CSharp/Day10_Dotnet/Day10_Dotnet/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day10_Dotnet
+{
+    public delegate void Empdelegate(string s); // declaration of a delegate with a specific signature
+    delegate int NumberChanger(int n);
+    delegate void Message();
+    class Employee
+    {
+        public static void accept(string name)
+        {
+            Console.WriteLine("Hello "+ name);
+        }
+
+        public void display(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public void ShowEmp()
+        {
+            Console.WriteLine("Hello All Employees"); 
+        }
+    }
+
+    class Numbers
+    {
+        static int num = 10;
+
+        public static int AddNum(int p)
+        {
+            num += p;
+            return num;
+        }
+
+        public static int MultiplyNum(int q)
+        {
+            num *= q;
+            return num;
+        }
+
+        public static int getNum()
+        {
+            return num;
+        }
+
+        public static void Showmessage()
+        {
+            Console.WriteLine("Hi, Welcome to delegates .."); 
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiating the implementing class
+            Employee emp = new Employee();
+
+            //creating objects of the delegate
+            Empdelegate ed1 = new Empdelegate(Employee.accept); //assigning the name of the static function/method to the delegate object
+
+            Empdelegate ed2 = new Empdelegate(emp.display); //assigning the name of the non-static function/method to the delegate object
+            //now let us invoke the delegate
+            ed1.Invoke("Amanullah");
+            ed2("This is a class on Delegates");
+            Console.WriteLine("--------- Example 2 Delegates ----------");
+            NumberChanger nc1 = new NumberChanger(Numbers.AddNum);
+            NumberChanger nc2 = new NumberChanger(Numbers.MultiplyNum);
+
+            //calling the methods using the delegate objects
+            nc1(25);
+            Console.WriteLine("Value of Num :{0}", Numbers.getNum());
+            nc2(5);
+            Console.WriteLine("Value of Num :{0}", Numbers.getNum());
+
+            Console.WriteLine("---------- Multicasting of delegates ---------");
+            //multicasting of delegates
+            Message mdel1 = new Message(Numbers.Showmessage);
+            Message mdel2 = new Message(emp.ShowEmp);
+            Message mdel = mdel1 + mdel2; //multicasting 
+            mdel();
+            Console.Read();
+
+        }
+    }
+}
