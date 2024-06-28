b940e74bc74c35b2d95353eb99d1450aba0db06b
BanurekhaV <rekhasairam@hotmail.com>
2024-06-28 17:26:57 +0530

    Day 12 dotnet

---
+++ CSharp/Day12_Dotnet/Day12_Dotnet/MultiThread.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Day12_Dotnet
+{
+    class MultiThread
+    {
+        static void Main()
+        {
+            Console.WriteLine("Main Thread Started....");
+
+            //let us create new threads
+
+            Thread t1 = new Thread(Method1)
+            {
+                Name = "Thread 1"
+            };
+
+            Thread t2 = new Thread(Method2)
+            {
+                Name = "Thread 2"
+            };
+
+            Thread t3 = new Thread(Method3)
+            {
+                Name = "Thread 3"
+            };
+            t1.Start();
+            t2.Start();
+            t3.Start();
+            Console.Read();
+
+        }
+        static void Method1()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 1 on Thread {0} ran {1} no of times : " ,Thread.CurrentThread.Name, i);
+            }
+
+        }
+        static void Method2()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 2 : no of times : " + i);
+                if (i == 3)
+                {
+                    Console.WriteLine("Performing some other Large Task....");
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Completed large task..");
+                }
+            }
+
+        }
+        static void Method3()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 3 : no of times : " + i);
+            }
+
+        }
+    }
+}
---
+++ CSharp/Day12_Dotnet/Day12_Dotnet/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Day12_Dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = Thread.CurrentThread;
+            t.Name = "Main Thread";
+            Console.WriteLine("Current Executing Thread : " + t.Name);
+            Console.WriteLine("Current Thread" + Thread.CurrentThread.Name);
+
+            Method1();
+            Method2();
+            Method3();
+            Console.Read();
+        }
+
+        static void Method1()
+        {
+             for(int i=0; i<=5; i++)
+            {
+                Console.WriteLine("Method 1 : no of times : "+ i);
+            }
+
+        }
+        static void Method2()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 2 : no of times : " + i);
+                if(i==3)
+                {
+                    Console.WriteLine("Performing some other Large Task....");
+                    Thread.Sleep(10000);
+                }
+            }
+
+        }
+        static void Method3()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine("Method 3 : no of times : " + i);
+            }
+
+        }
+    }
+}
