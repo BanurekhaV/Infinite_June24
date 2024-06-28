618239313b8466d6712980a09fd7fb035331530b
BanurekhaV <rekhasairam@hotmail.com>
2024-06-28 11:39:51 +0530

    part3 day11 dotnet

---
+++ CSharp/Day11_Dotnet/Day11_Dotnet/Files_Eg1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day11_Dotnet
+{
+    class Files_Eg1
+    {
+        static void Main()
+        {
+            // WriteBinary();
+            // ReadBinary();
+            FilesEg2 file2 = new FilesEg2();
+            file2.ReadData();
+            Console.Read();
+        }
+        static void WriteBinary()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open("C:\\Banu\\Infinite\\Batch_June24\\binfile.bin", FileMode.Create)))
+            {
+                //let us write now
+                writer.Write("0x802348904");
+                writer.Write(true);
+                writer.Write("Windows Binary File");
+            }
+        }
+
+        static void ReadBinary()
+        {
+            using(BinaryReader reader = new BinaryReader(File.Open("C:\\Banu\\Infinite\\Batch_June24\\binfile.bin",FileMode.Open)))
+            {
+                Console.WriteLine("Address Code : " + reader.ReadString());
+                Console.WriteLine("Boolean Value : " + reader.ReadBoolean());
+                Console.WriteLine("Message : " + reader.ReadString());
+            }
+        }
+    }
+
+    class FilesEg2
+    {
+        public void ReadData()
+        {
+            FileStream fs = new FileStream(@"C:\Banu\Infinite\Batch_June24\Git Steps.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            /*we can position the file pointer at any location by seeking no,
+             * of bytes. But here we want to position it from the beginning*/
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+
+            string str = sr.ReadLine();
+
+            while(str != null)
+            {
+                Console.WriteLine("{0}", str);
+                str = sr.ReadLine();
+            }
+
+            sr.Close();
+            fs.Close();
+        }
+    }
+}
--- CSharp/Day11_Dotnet/Day11_Dotnet/UsingBlock.cs
+++ CSharp/Day11_Dotnet/Day11_Dotnet/UsingBlock.cs
@@ -25,11 +25,50 @@ namespace Day11_Dotnet
                 } while (finaldata != null);
             }
 
-            using (var streamreader = new StreamReader(@"C:\Banu\Infinite\Batch_June24\Basics.txt"))
+            //using (var streamreader = new StreamReader(@"C:\Banu\Infinite\Batch_June24\Basics.txt"))
+            //{
+            //    Console.WriteLine(streamreader.ReadToEnd());
+            //}
+
+            //invoking members of the class that is implementing Dispose() of IDisposable interface
+            Console.WriteLine("***************************");
+            using(var trydisposable = new TryDisposable())
             {
-                Console.WriteLine(streamreader.ReadToEnd());
+                trydisposable.DoSomething();
             }
             Console.Read();
+        }
+
+        public void ReadfromFile()
+        {
+            var streamr = new StreamReader(@"C:\Banu\Infinite\Batch_June24\Basics.txt");
+            try
+            {
+                Console.WriteLine(streamr.ReadToEnd());
+            }
+            finally
+            {
+                if (streamr != null)
+                    streamr.Dispose();
+            }
+        }
+    }
+
+    class TryDisposable : IDisposable
+    {
+        public TryDisposable()
+        {
+            Console.WriteLine("Allocating Resources...");
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Doing some work using the resources...");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Releasing all Resources...");
         }
     }
 }
