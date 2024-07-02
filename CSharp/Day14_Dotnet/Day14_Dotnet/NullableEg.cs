417338411227f2b84b93f70e14f5982d41e43248
BanurekhaV <rekhasairam@hotmail.com>
2024-07-02 13:12:39 +0530

    Day14 dotnet

---
+++ CSharp/Day14_Dotnet/Day14_Dotnet/NullableEg.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Dotnet
+{
+    class NullableEg
+    {
+        static void Main()
+        {
+            Nullable<int> i = 120;
+            if(i.HasValue)
+                Console.WriteLine(i.Value);
+            else
+                Console.WriteLine("Null");
+
+            Console.WriteLine("null condition-------");
+            NullableCheck();
+            Console.Read();
+        }
+
+        public static void NullableCheck()
+        {
+            NewEmployees newemp = new NewEmployees() { ID = 5, Name = "Tanmayee" };//null;
+            
+           string ename = (newemp != null) ? newemp.Name : null;
+            //or we can simplify it as below
+            string empname = newemp?.Name;
+            Console.WriteLine(ename + " "+ empname);
+        }
+    }
+}
---
+++ CSharp/Day14_Dotnet/Day14_Dotnet/ThrowsEg.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Dotnet
+{
+    class ThrowsEg
+    {
+        static void Main()
+        {
+            var a = Divide(10, 2);
+            Console.WriteLine(a);
+            Console.WriteLine(ExpressionBodiedEg.IsLeapYear());
+            Console.WriteLine(ExpressionBodiedEg.IsLeapYearwithExp());
+            Console.WriteLine(ExpressionBodiedEg.LeapYear());
+            Console.WriteLine("-------------------");
+            Console.WriteLine(ExpressionBodiedEg.Square(8));
+            Console.WriteLine(ExpressionBodiedEg.CalcOps(10,5));
+            Console.Read();
+        }
+
+        public static double Divide(int x, int y)
+        {
+            //directly throw an exception
+            return y != 0 ? x % y : throw new DivideByZeroException();
+        }
+    }
+
+    class ExpressionBodiedEg
+    {
+        public static int year = 2016;
+
+        //without expression bodied member
+        public static string IsLeapYear()
+        {
+            return "\n Is " + year + " a Leap Year ?" + DateTime.IsLeapYear(year);
+        }
+        //with expression bodied member
+        public static string IsLeapYearwithExp() => "\n Is " + year + " a Leap Year ?" + DateTime.IsLeapYear(year);
+        //or as below
+        public static string LeapYear() => $"Is {year} a Leap Year :?" + DateTime.IsLeapYear(year);
+
+        public static int Square(int s) => s * s;
+        public static int CalcOps(int a, int b) => ((a + b) + (a - b) + (a * b) + (a / b));
+    }
+   
+    
+}
