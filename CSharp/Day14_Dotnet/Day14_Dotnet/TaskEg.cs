9695ad5b87cf8c9278e18e6794f867a6fd7df73c
BanurekhaV <rekhasairam@hotmail.com>
2024-07-02 18:43:39 +0530

    Day14 dotnet

---
+++ CSharp/Day14_Dotnet/Day14_Dotnet/TaskEg.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14_Dotnet
+{
+    class Accounts
+    {
+        int balance;
+
+        public Accounts(int a)
+        {
+            balance = a;
+        }
+
+        public void Credit(int amt)
+        {
+            balance += amt;
+            Console.WriteLine(balance);
+        }
+        public void Debit(int amt)
+        {
+            balance -= amt;
+            Console.WriteLine(balance);
+        }
+    }
+    class TaskEg
+    {
+        static void Main()
+        {
+            var account = new Accounts(1000);
+
+            
+            var task = new Task[10];
+            for(int i= 0; i<task.Length; i++)
+            {
+                task[i] = Task.Run(() => RandomlyUpdate(account));
+            }
+            Task.WaitAll(task);
+            Console.Read();
+        }
+
+        static void RandomlyUpdate(Accounts account)
+        {
+            var rnd = new Random();
+            for(int i=0; i<10; i++)
+            {
+                var amount = rnd.Next(1, 100);
+                bool docredit = rnd.NextDouble() < 0.5;
+                if (docredit)
+                {
+                    account.Credit(amount);
+                }
+                else
+                    account.Debit(amount);
+            }
+        }
+    }
+}
--- CSharp/Day14_Dotnet/Day14_Dotnet/TuplesEg.cs
+++ CSharp/Day14_Dotnet/Day14_Dotnet/TuplesEg.cs
@@ -6,12 +6,13 @@ namespace Day14_Dotnet
 {
     class TuplesEg
     {
-        //if we want a method to return min,max,average from a given collection
-        static (int,double)GetResults(List<double>numbers)
+        //if we want a method to return many values from a given collection
+        //tuple values displayed using standard item collection viz item1, item2 ... etc
+        static (int, double) GetResults(List<double> numbers)
         {
             int count = 0;
             double sum = 0;
-            foreach(var v in numbers)
+            foreach (var v in numbers)
             {
                 count++;
                 sum += v;
@@ -19,13 +20,56 @@ namespace Day14_Dotnet
             return (count, sum);
         }
 
+        //tuples with named return parameters
+
+        static (int count, double sum) GetResults1(List<double> numbers)
+        {
+            int count = 0;
+            double sum = 0;
+            foreach (var v in numbers)
+            {
+                count++;
+                sum += v;
+            }
+            return (count, sum);
+        }
         static void Main()
         {
+            
+            Console.WriteLine("----Tuples befor 7.0---------");
+            //tuples before C#7.0
+            var values = new List<double>() { 10, 20, 30, 40, 50 };
+            Tuple<int, double> t = GetValues(values);
+            Console.WriteLine($"There are {t.Item1} elements and their Sum is {t.Item2}");
+
+            //tuples with 7.0
             List<double> numbers = new List<double> { 52, 45, 120, 56, 98, 304, 20, 69 };
             var results = GetResults(numbers);
-            Console.WriteLine($"Count of Number :{ results.Item1}, Sum of Numbers : {results.Item2} ");
+            Console.WriteLine($"Count of Numbers :{ results.Item1}, Sum of Numbers : {results.Item2} ");
+
+            //with named parameters
+            var res = GetResults1(numbers);
+            Console.WriteLine($" Count of numbers {res.count}, and the Sum of Numbers :{res.sum}");
+
+            //explicit names to store the return values
+            var (countresult, sumresult) = GetResults1(numbers);
+            Console.WriteLine($" Count of numbers {countresult}, and the Sum of Numbers :{sumresult}");
             Console.Read();
-            Tuple
+
+        }
+
+        static Tuple<int,double>GetValues(IEnumerable<double> values)
+        {
+            int count = 0;
+            double sum = 0;
+            foreach(var v in values)
+            {
+                count++;
+                sum += v;
+            }
+            //creating a tuple instance using static create method with items
+            Tuple<int, double> t1 = Tuple.Create(count, sum);
+            return t1;
         }
     }
 }
