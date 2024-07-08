a1521477c860ee631871a5030d62b99b2ecbd293
BanurekhaV <rekhasairam@hotmail.com>
2024-07-08 12:12:48 +0530

    Nunit testing

---
+++ CSharp/Testing/ProjectTester/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using Testing;
+
+namespace ProjectTester
+{
+    [TestFixture]
+    public class Class1
+    {
+        //arrange
+        Program p = new Program();
+
+        [Test]
+        //1. test case
+        public void Test_Login()
+        {
+            //Act
+            string s1 = p.Login("Banurekha", "aaa");
+            string s2 = p.Login("", "");
+            string s3 = p.Login("Admin", "Admin@123");
+
+            //Assert
+            ClassicAssert.AreEqual("UserId or Password cannot be empty", s2);
+            ClassicAssert.AreEqual("Incorrect User Id or Password", s1);
+            ClassicAssert.AreEqual("Welcome Admin", s3);
+        }
+    }
+}
---
+++ CSharp/Testing/Testing/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Gender { get; set; }
+    }
+    public class Program
+    {
+        public string Login(string userid, string password)
+        {
+            if (string.IsNullOrEmpty(userid) || string.IsNullOrEmpty(password))
+            {
+                return "UserId or Password cannot be empty";
+            }
+            else if (userid == "Admin" && password == "Admin@123")
+            {
+                return "Welcome Admin";
+            }
+            else
+                return "Incorrect User Id or Password";
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
