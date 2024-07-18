210f843d40bad09410d183909d073292dde87433
BanurekhaV <rekhasairam@hotmail.com>
2024-07-18 18:33:05 +0530

    Ado connected

--- ADO/Connected_Eg1/Connected_Eg1/Program.cs
+++ ADO/Connected_Eg1/Connected_Eg1/Program.cs
@@ -15,7 +15,9 @@ namespace Connected_Eg1
         public static SqlDataReader dr;
         static void Main(string[] args)
         {
-            SelectData();
+            InsertData();  
+          //  SelectData();
+           // Select_With_Parameters();
             Console.Read();
         }
 
@@ -32,12 +34,76 @@ namespace Connected_Eg1
         public static void SelectData()
         {
             con = getConnection();
-            cmd = new SqlCommand("select * from tblemployees", con);
+            // option 1
+            // cmd = new SqlCommand("select * from tblemployees", con); command text with connection object
+            //2 option 
+            cmd = new SqlCommand("Select * from tblemployees"); //only command text
+
+            
+            cmd.Connection = con;  
             dr = cmd.ExecuteReader();
 
             while(dr.Read())
             {
                 Console.WriteLine(dr[0] + " | " + dr[1] + " | " + dr[2] + " | " + dr[3] + " | " + dr[4]);
+            }
+        }
+
+        //select with a where predicate
+
+        public static void Select_With_Parameters()
+        {
+            con = getConnection();
+            int deptid;
+            Console.WriteLine("Mention the DeptNo to select Employees Data :");
+            deptid = Convert.ToInt32(Console.ReadLine());
+            cmd = new SqlCommand("select * from tblemployees where deptno = @did");
+
+            //now we will associate the C# variable to that of sql variable along with data
+            cmd.Parameters.AddWithValue("@did", deptid);
+            cmd.Connection = con;
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.WriteLine(dr[0] + " | " + dr[1] + " | " + dr[2] + " | " + dr[3] + " | " + dr[4]);
+            }
+        }
+
+        //insert a record
+        public static void InsertData()
+        {
+            con = getConnection();
+            int deptno;
+            string deptname;
+            float budget;
+            string location;
+            Console.WriteLine("enter department details");
+            deptno = Convert.ToInt32(Console.ReadLine());
+            deptname = Console.ReadLine();
+            budget = Convert.ToSingle(Console.ReadLine());
+            location = Console.ReadLine();
+
+            cmd = new SqlCommand("Insert into tbldepartment values(@deptno,@deptname,@budget,@loc)",con);
+
+            cmd.Parameters.AddWithValue("@deptno", deptno);
+            cmd.Parameters.AddWithValue("@deptname", deptname);
+            cmd.Parameters.AddWithValue("@budget", budget);
+            cmd.Parameters.AddWithValue("@loc", location);
+
+            int result = cmd.ExecuteNonQuery();
+
+            if(result > 0)
+                Console.WriteLine("Insertion success");
+            else
+                Console.WriteLine("Could not insert data");
+
+            SqlCommand cmd1 = new SqlCommand("select * from tbldepartment", con);
+            dr = cmd1.ExecuteReader();
+            while(dr.Read())
+            {
+                Console.WriteLine("Deptnumber : {0}", dr[0]);
+                Console.WriteLine("Deptname : {0}", dr[1]);
             }
         }
     }
