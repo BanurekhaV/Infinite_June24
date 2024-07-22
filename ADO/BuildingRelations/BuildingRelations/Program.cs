80867a01b18daaa0e5026951c1954099ee3911f3
BanurekhaV <rekhasairam@hotmail.com>
2024-07-22 13:22:29 +0530

    building relations - dataset

--- ADO/BuildingRelations/BuildingRelations/Program.cs
+++ ADO/BuildingRelations/BuildingRelations/Program.cs
@@ -16,8 +16,8 @@ namespace BuildingRelations
             //3. define columns to the datatable
             DataColumn[] dcEmployees = new DataColumn[4];
             dcEmployees[0] = new DataColumn("Empcode", System.Type.GetType("System.Int32"));
-            dcEmployees[1] = new DataColumn("EmpName", System.Type.GetType("System.string"));
-            dcEmployees[2] = new DataColumn("EmpDept", System.Type.GetType("System.string"));
+            dcEmployees[1] = new DataColumn("EmpName", System.Type.GetType("System.String"));
+            dcEmployees[2] = new DataColumn("EmpDept", System.Type.GetType("System.String"));
             dcEmployees[3] = new DataColumn("EmpStatusID", System.Type.GetType("System.Int32"));
 
             //4. Add the above columns to the datatbale
@@ -76,6 +76,94 @@ namespace BuildingRelations
             drEmpRows["EmpStatusID"] = "4";
                        
             dtEmployees.Rows.Add(drEmpRows);
+
+            //7. create another table
+            DataTable dtEmpStatus = new DataTable("EmployeeStatus");
+
+            //8. Columns for table 2
+            DataColumn[] dcStatus = new DataColumn[2];
+            dcStatus[0] = new DataColumn("EmpStatusID", System.Type.GetType("System.Int32"));
+            dcStatus[1] = new DataColumn("EmpStatus", System.Type.GetType("System.String"));
+
+            //9. adding columns to the table
+            dtEmpStatus.Columns.Add(dcStatus[0]);
+            dtEmpStatus.Columns.Add(dcStatus[1]);
+
+            //10. rows for table 2 and add it to the tables by repeating 
+
+            DataRow drStatus = dtEmpStatus.NewRow();
+
+            drStatus["EmpStatusID"] = "1";
+            drStatus["EmpStatus"] = "Full Time";
+
+            dtEmpStatus.Rows.Add(drStatus);
+
+            drStatus = dtEmpStatus.NewRow();
+
+            drStatus["EmpStatusID"] = "2";
+            drStatus["EmpStatus"] = "Part Time";
+
+            dtEmpStatus.Rows.Add(drStatus);
+
+            drStatus = dtEmpStatus.NewRow();
+
+            drStatus["EmpStatusID"] = "3";
+            drStatus["EmpStatus"] = "Contract";
+
+            dtEmpStatus.Rows.Add(drStatus);
+            drStatus = dtEmpStatus.NewRow();
+
+            drStatus["EmpStatusID"] = "4";
+            drStatus["EmpStatus"] = "Intern";
+
+            dtEmpStatus.Rows.Add(drStatus);
+
+            //11. add both the tables to the dataset
+
+            dsEmployment.Tables.Add(dtEmployees);
+            dsEmployment.Tables.Add(dtEmpStatus);
+
+            //12. setting relationship between 2 datatables of the dataset
+
+            //12.1 primary key and foreign key
+            DataColumn parent = dsEmployment.Tables["EmployeeStatus"].Columns["EmpStatusID"];
+            DataColumn child = dsEmployment.Tables["Employees"].Columns["EmpStatusID"];
+
+            //12.2 setting the relation
+            DataRelation emprel = new DataRelation("EmpStatusRelation", parent, child);
+
+            //12.3 adding relation to the dataset
+            dsEmployment.Relations.Add(emprel);
+
+
+            //13. Display Data as per the relationship
+            Console.WriteLine("===================================================================");
+            Console.WriteLine("Status ID             |             Employee Status     ");
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            foreach(DataRow row in dsEmployment.Tables["EmployeeStatus"].Rows)
+            
+                Console.WriteLine("{0}             |          {1}", row["EmpStatusID"], row["EmpStatus"]);
+                Console.WriteLine("---------------------------------------------------------------------");
+                Console.WriteLine("EmpCode \t     |      Empname\t       |    Department\t        |        EmployeeStatus");
+                Console.WriteLine("---------------------------------------------------------------------------------------");
+
+            foreach (DataRow row in dsEmployment.Tables["Employees"].Rows)
+            {
+                //Console.WriteLine("{0}    \t     |      {1}   \t       |     {2}    \t      |       {3}", row["Empcode"],
+                //    row["EmpName"], row["EmpDept"], row["EmpStatusID"]);
+
+
+                //if we want the type of employement as a string and not id
+                Console.WriteLine("******************************************************");
+                int irow = int.Parse(row["EmpStatusID"].ToString());
+
+                DataRow currentrow = dsEmployment.Tables["EmployeeStatus"].Rows[irow - 1];
+                Console.WriteLine("{0}    \t     |      {1}   \t       |     {2}    \t      |       {3}", row["Empcode"],
+                    row["EmpName"], row["EmpDept"], currentrow["EmpStatus"]);
+            }
+            Console.Read();
+
         }
     }
 }
