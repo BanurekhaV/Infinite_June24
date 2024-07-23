8cdb1052958e372251350c8951d7a7d7bf433faa
BanurekhaV <rekhasairam@hotmail.com>
2024-07-23 18:39:05 +0530

    Linq to sql - sql metal

---
+++ ADO/Linq_To_Sql_SqlMetal/Linq_To_Sql_SqlMetal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Linq_To_Sql_SqlMetal
+{
+    class Program
+    {
+        static string connstr = ConfigurationManager.ConnectionStrings["NwindConnstr"].ConnectionString;
+        static NorthwindContext db = new NorthwindContext(connstr);
+        static void Main(string[] args)
+        {
+            var cust = from c in db.Customers
+                       orderby c.ContactName
+                       select c;
+
+            foreach(var v in cust)
+            {
+                Console.WriteLine($"{v.CustomerID}, {v.ContactName}");
+            }
+            Console.Read();
+        }
+    }
+}
