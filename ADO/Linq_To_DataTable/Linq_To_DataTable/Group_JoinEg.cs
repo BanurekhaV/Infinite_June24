265601f899b920c94395be235ecaf5169cf11dc7
BanurekhaV <rekhasairam@hotmail.com>
2024-07-23 13:32:45 +0530

    Linq Queries

---
+++ ADO/Linq_To_DataTable/Linq_To_DataTable/Group_JoinEg.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_To_DataTable
+{
+    class Person
+    {
+        public int PId { get; set; }
+        public string pName { get; set; }
+    }
+
+    class Course
+    {
+        public int CId { get; set; }
+        public string cName { get; set; }
+    }
+    class Group_JoinEg
+    {
+        static void Main()
+        {
+            // Simple_join();
+            Sample_GroupJoin();
+            Console.Read();
+        }
+
+        static void Sample_GroupJoin()
+        {
+            Person[] p = new Person[]
+            {
+                new Person{PId= 1, pName="Sam"},
+                new Person{PId= 2, pName="Kom"},
+                new Person{PId= 2, pName="Rom"},
+                new Person{PId= 1, pName="Jam"},
+                new Person{PId= 1, pName="Dam"},
+                new Person{PId= 3, pName="Ram"},
+            };
+
+            Course[] c = new Course[]
+            {
+                new Course{CId = 1, cName = "Ado.Net"},
+                new Course{CId = 2, cName = "C#.Net"},
+                new Course{CId = 3, cName = "SQL"}
+            };
+
+            var result = c.GroupJoin(p, crs => crs.CId, pers => pers.PId, (cr, ps) => new { key = cr.cName, ps });
+
+            Console.WriteLine("Group-Joined List of Persons who have taken various courses ");
+
+            foreach(var item in result)
+            {
+                Console.WriteLine(item.key);
+             
+
+                foreach (var per in item.ps)
+                {
+                 
+                    Console.WriteLine(per.PId + " " + per.pName);
+                }
+            }
+        }
+        static void Simple_join()  //inner joins
+        {
+            string[] str1 = { "India", "Japan", "US", "Korea", "Russia" };
+            string[] str2 = { "China", "Pakistan", "Japan", "UK", "India", "Korea" };
+
+            var result = str1.Join(str2, s1 => s1, s2 => s2, (s1, s2) => s2);
+
+            Console.WriteLine("Post Joining the strings...");
+
+            foreach(var country in result)
+            {
+                Console.WriteLine(country);
+            }
+        }
+    }
+}
---
+++ ADO/Linq_To_DataTable/Linq_To_DataTable/Linq_Operators.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_To_DataTable
+{
+    class Linq_Operators
+    {
+        public static void Main()
+        {
+            //Element_At();
+            // First_operators();
+            //  SingleEg();
+            //SortingEg();
+              Sample_Thenby();
+            // Sample_Groupby();
+           // Simple_Seed_and_Aggregates();
+            Console.Read();
+        }
+
+        static void Element_At()
+        {
+            string[] fruits = { "Apple", "Oranges", "Bananas", "Kiwi", "Grapes" };
+
+            var result = fruits.ElementAt(3);
+           //  result = fruits.ElementAt(5);//throws out of range exception
+            result = fruits.ElementAtOrDefault(5);
+            Console.WriteLine(result);
+        }
+
+        static void First_operators()
+        {
+            string[] colors = { "red", "blue", "green", "yellow" };
+            Console.WriteLine(colors.First());
+            Console.WriteLine(colors.Last());
+            string[] colors1 = { };
+            Console.WriteLine(colors1.FirstOrDefault());
+            Console.WriteLine(colors1.LastOrDefault());
+        }
+
+        static void SingleEg()
+        {
+            string[] names1 = { "Infinte Limited" };
+            string[] names2 = { "Joe Biden", "Donald Trump", "Obama" };
+            string[] empty = { };
+
+            Console.WriteLine(names1.SingleOrDefault()); //works only if the collection has a element
+           // Console.WriteLine(names2.SingleOrDefault()); //throws exception if collection has more elements
+            Console.WriteLine(empty.SingleOrDefault()); //avoids exception if the collection has no elements
+        }
+
+
+        public static void SortingEg()
+        {
+            string[] name = { "Joe Biden", "Donald Trump", "Obama" };
+
+            //sorting in ascending order
+            var namesort = name.OrderBy(n => n);
+
+            foreach(var v in namesort)
+            {
+                Console.WriteLine(v);
+            }
+
+            Console.WriteLine("----------descending sort------------------");
+            namesort = name.OrderByDescending(n => n);
+
+            foreach (var v in namesort)
+            {
+                Console.WriteLine(v);
+            }
+                     
+        }
+
+        //multiple sorting
+        //orderby, thenby
+        //orderbydescending, thenbydescending
+        static void Sample_Thenby()
+        {
+            string[] capitals = {"Delhi","Ambal","Mumbai","abcde","Nagpur","Bangalore","Hyderabad","Chennai",
+            "Ahmedabad"};
+
+            var result = capitals.OrderBy(c => c.Length).ThenBy(c => c);
+            Console.WriteLine("---------Multiple regular sort----------");
+            foreach(string s in result)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("---------multiple descending sort--------");
+
+            result = capitals.OrderByDescending(c => c.Length).ThenByDescending(c => c);
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        //group by
+
+        static void Sample_Groupby()
+        {
+            int[] numbers = { 10, 15, 20, 25, 30, 35, 42 };
+                             
+            var result = numbers.GroupBy(num => (num % 10 == 0));
+
+            foreach(IGrouping<bool,int> gp in result)
+            {
+                if(gp.Key==true)
+                {
+                    Console.WriteLine("Group 1 - Divisible by 10");
+                }
+                else
+                    Console.WriteLine("Group 2 - Not Divisible by 10");
+
+                foreach(int n in gp)
+                { 
+                    Console.WriteLine(n); 
+                }
+            }
+
+        }
+
+        public static void Simple_Seed_and_Aggregates()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+           
+            Console.WriteLine("Sum is :{0}", numbers.Sum());  //15
+            //aggregates with seed value
+            var res = numbers.Aggregate(10, (a, b) => a + b);   //25
+            Console.WriteLine("Aggregates with Seed value :{0}", res);
+            var result = numbers.Aggregate((a, b) => a * b);   //120
+            Console.WriteLine("Aggregates 2 {0}", result);
+        }
+    }
+}
