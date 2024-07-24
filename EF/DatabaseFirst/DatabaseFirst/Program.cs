9a11465e2084a40768979952f159fbfc336d38aa
BanurekhaV <rekhasairam@hotmail.com>
2024-07-24 15:48:45 +0530

    Ef db first

---
+++ EF/DatabaseFirst/DatabaseFirst/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst
+{
+    class Program
+    {
+        //let us create an object of the context class
+        static InfiniteDBEntities db = new InfiniteDBEntities();
+        static void Main(string[] args)
+        {
+            var emp = db.tblemployees.ToList();
+
+            foreach(var e in emp)
+            {
+                Console.WriteLine(e.Empid + " " + e.Empname +  " "+ e.Salary) ;
+            }
+
+            Console.Read();
+        }
+    }
+}
