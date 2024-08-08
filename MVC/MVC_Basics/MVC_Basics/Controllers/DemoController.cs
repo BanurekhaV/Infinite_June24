fef10e506d815135e29022a2fd7caf97a6bdb07e
BanurekhaV <rekhasairam@hotmail.com>
2024-08-08 13:45:09 +0530

    mvc 1

--- MVC/MVC_Basics/MVC_Basics/Controllers/DemoController.cs
+++ MVC/MVC_Basics/MVC_Basics/Controllers/DemoController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Basics.Models;
 
 namespace MVC_Basics.Controllers
 {
@@ -25,6 +26,40 @@ namespace MVC_Basics.Controllers
         public ViewResult Viewmethod()
         {
             return View();
+        }
+
+        //3. ContentResult method
+        public ContentResult contentmethod()
+        {
+            return Content("Happy Day!!", "text/plain");
+           // return Content("<h1>Good Morning All</h1>", "text/html");
+        }
+
+        //4. empty result method returns nothing
+        //can be used to redirect to another action method or can be made non actionable
+        [NonAction]
+        public EmptyResult Empty()
+        {
+            int amt = 45000;
+            float si = (amt * 3 * 2) / 100;
+            return new EmptyResult();
+        }
+
+        //5. jsonresult method
+        public JsonResult EmpData()
+        {
+            Employee employee = new Employee();
+            employee.Id = 101;
+            employee.Name = "Apurupa";
+            employee.Age = 26;
+            return Json(employee,JsonRequestBehavior.AllowGet);
+        }
+
+        //6. Redirect method
+        public ActionResult redirectmethod()
+        {
+           // return RedirectToAction("Empdata");  //redirecting to the action method of the same controller
+            return RedirectToAction("About", "Home");  //redirecting to the action method of other controller
         }
     }
 }
---
+++ MVC/MVC_Basics/MVC_Basics/Controllers/HRController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Basics.Models;
+
+namespace MVC_Basics.Controllers
+{
+    public class HRController : Controller
+    {
+        // GET: HR
+        //3. 
+        public ActionResult Index()
+        {
+            List<Department> d = new List<Department>();
+            d.Add(new Department { Id = 10, Dname = "CSE" });
+            d.Add(new Department { Id = 11, Dname = "IT" });
+            d.Add(new Department { Id = 12, Dname = "ECE" });
+            d.Add(new Department { Id = 13, Dname = "EEE" });
+            return View("DepartmentList",d);
+        }
+
+        //1. binding a model object to the view
+        public ActionResult DisplayEmployee()
+        {
+            Employee e = new Employee() { Id = 1, Name = "Infinite", Age = 25 };
+            return View(e);  //view is bound to an model object
+        }
+
+        //2. 
+        public ActionResult EmployeesList()
+        {
+            List<Employee> lstemp = new List<Employee>()
+            {
+                new Employee{Id=100,Name="Adi", Age=22},
+                new Employee{Id=101,Name="Eswar", Age=22},
+                new Employee{Id=102,Name="Naman", Age=21},
+            };
+            return View(lstemp);
+        }
+
+        //4.
+        public ActionResult DepartmentList(Department dept)
+        {
+            return View(dept);
+        }
+
+
+    }
+}
