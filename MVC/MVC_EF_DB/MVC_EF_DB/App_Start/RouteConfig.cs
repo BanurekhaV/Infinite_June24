f1625580f4bb26d853b060dd3695cab643809630
BanurekhaV <rekhasairam@hotmail.com>
2024-08-09 11:37:20 +0530

    mvc -ef

---
+++ MVC/MVC_EF_DB/MVC_EF_DB/App_Start/RouteConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MVC_EF_DB
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "Cat",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Category", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ MVC/MVC_EF_DB/MVC_EF_DB/Controllers/CategoryController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_EF_DB.Models;
+
+namespace MVC_EF_DB.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //create an object of the context class
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Category
+
+        //2. creating a scaffolded View for fetching categories 
+        public ActionResult Index()
+        {
+            List<Category> catlist = db.Categories.ToList();
+            return View(catlist);
+        }
+
+        //1. fetch data from the categories DbSet
+        public ActionResult GetCategoryDetails()
+        {
+            List<Category> catlist = db.Categories.ToList();
+            return View(catlist);
+        }
+
+        //3. Inserting a new category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //passing data from the view to the controller using model object
+        [HttpPost]
+        public ActionResult Create(Category c)
+        {
+            db.Categories.Add(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //4. delete a category
+        public ActionResult Delete(int Id)
+        {
+            Category cat = db.Categories.Find(Id);
+            return View(cat);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteCategory(int Id)
+        {
+            Category c = db.Categories.Find(Id);
+            db.Categories.Remove(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //5. Edit a category
+        public ActionResult Update(int Id)
+        {
+            Category c = db.Categories.Find(Id);
+            return View(c);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Category c)
+        {
+            Category cat = db.Categories.Find(c.CategoryID);
+            cat.CategoryName = c.CategoryName;
+            cat.Description = c.Description;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
