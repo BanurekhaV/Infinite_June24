cee384ed1b6002d9960daf4330122275bec16569
BanurekhaV <rekhasairam@hotmail.com>
2024-08-09 16:53:27 +0530

    mvc -ef

---
+++ MVC/MVC_EF_DB/MVC_EF_DB/Controllers/NavigationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_EF_DB.Models;
+
+namespace MVC_EF_DB.Controllers
+{
+    public class NavigationController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Navigation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //1. Fetching Data from Multiple tables using navigation property
+        public ActionResult MultipleData()
+        {
+             return View(db.Orders.ToList());            
+        }
+
+        public ActionResult MoreData()
+        {
+            dynamic mymodel = new ExpandoObject();
+            mymodel.Orderlist = db.Orders.ToList();
+            mymodel.Detaillist = db.Order_Details.ToList();
+            mymodel.Custlist = db.Customers.ToList();
+            mymodel.Emplist = db.Employees.ToList();
+            return View(mymodel);
+        }
+    }
+
+}
--- MVC/MVC_EF_DB/MVC_EF_DB/Controllers/ShipperController.cs
+++ MVC/MVC_EF_DB/MVC_EF_DB/Controllers/ShipperController.cs
@@ -21,7 +21,7 @@ namespace MVC_EF_DB.Controllers
             return View();
         }
 
-        // pass data from the view to the controller using forms collection
+        //1.  pass data from the view to the controller using forms collection
         public ActionResult Create(FormCollection frm)
         {
             Shipper s = new Shipper();
@@ -31,6 +31,39 @@ namespace MVC_EF_DB.Controllers
             db.Shippers.Add(s);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        //2. Passing data from the view to the controller using parameters
+        //public ActionResult Create(string CompanyName, string Phone)
+        //{
+        //    Shipper s = new Shipper();
+        //    s.CompanyName = CompanyName;
+        //    s.Phone = Phone;
+        //    db.Shippers.Add(s);
+        //    db.SaveChanges();
+        //    return RedirectToAction("index");
+        //}
+
+        public ActionResult Edit(int Id)
+        {
+            Shipper s = db.Shippers.Find(Id);
+            return View(s);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(FormCollection frm)
+        {
+            Shipper s = db.Shippers.Find(Convert.ToInt32(frm["ShipperID"]));
+            s.CompanyName = frm["CompanyName"].ToString();
+            s.Phone = frm["Phone"].ToString();
+            db.SaveChanges();
+            return RedirectToAction("index");
+        }
+
+        //calling a stored procedure with input
+        public ActionResult SP_WithInput()
+        {
+            return View(db.CustOrdersOrders("ALFKI"));
         }
     }
 }
