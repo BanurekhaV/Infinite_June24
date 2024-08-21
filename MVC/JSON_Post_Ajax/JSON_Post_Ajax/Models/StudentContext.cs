66a964cdff82b22a588fd2e3868adc3c8f520149
BanurekhaV <rekhasairam@hotmail.com>
2024-08-21 18:09:09 +0530

    AjaxPost

--- MVC/JSON_Post_Ajax/JSON_Post_Ajax/Models/StudentContext.cs
+++ MVC/JSON_Post_Ajax/JSON_Post_Ajax/Models/StudentContext.cs
@@ -8,6 +8,7 @@ namespace JSON_Post_Ajax.Models
 {
     public class StudentContext :DbContext
     {
+        public StudentContext() : base("name=StdContext") { }
         public DbSet<Student> Students { get; set; }
     }
 }
